5cba811b47aadb73a7711529d2c5e818437d1bd4
StevenDepa <stefano.depa91@gmail.com>
2015-05-25 22:58:54 +0200

    Corrected error in TeamManager

--- src/game/model/TeamManager.java
+++ src/game/model/TeamManager.java
@@ -14,16 +14,14 @@ public class TeamManager {
 	public static final int NUMBER_OF_TEAMS = 1;
 
 	private ArrayList<Team> teams = new ArrayList<Team>();
-	public int numberOfTeams;
 
 	public TeamManager() {
 		generateTeams();
-		numberOfTeams = NUMBER_OF_TEAMS;
+		
 	}
 	
 	public TeamManager(int numbersOfTeam) {
 		generateTeams();
-		numberOfTeams = NUMBER_OF_TEAMS;
 	}
 
 	/**
@@ -31,7 +29,7 @@ public class TeamManager {
 	 */
 	private void generateTeams() {
 
-		for (int i = 0; i < numberOfTeams; i++) {
+		for (int i = 0; i < NUMBER_OF_TEAMS; i++) {
 			Team team = new Team();
 			team.setTeamName("Team " + (teams.size() + 1));
 			team.setRandomTeamColor();
